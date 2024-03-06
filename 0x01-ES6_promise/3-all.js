5fe310353afb6fd5519606893cfe3997e8783d5c
abouelfaraj <aef.ayoub@gmail.com>
2024-03-06 20:48:41 +0000

    cmt 3-all.js

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,15 @@
+/* */
+import { uploadPhoto, createUser } from './utils';
+
+export default function handleProfileSignup() {
+  return Promise.all([uploadPhoto(), createUser()])
+  .then((result) => {
+	  console.log(
+  `${reslt[0].body} ${resut[1].firstName} ${reslt[1].lastName}`,
+	  );
+  })
+
+  .catch((error) => {
+    consolelo('Signup system offline';
+  });
+}
