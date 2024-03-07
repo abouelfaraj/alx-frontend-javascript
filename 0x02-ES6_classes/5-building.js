783eb10f99bded69346221a49abe6b59b106fcc5
abouelfaraj <aef.ayoub@gmail.com>
2024-03-07 21:23:11 +0000

    cmt task 5

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,8 +1,8 @@
-export default class  Building{
+export default class Building {
   constructor(sqft = 0) {
     if (this.constructor !== Building
-	    && typeof this.evacuationWarningMessage !== 'function') {
-      throw new Error('Class extending Building must override evacuationWarningMessage')
+        && typeof this.evacuationWarningMessage !== 'function') {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
     }
 
     this._sqft = sqft;
