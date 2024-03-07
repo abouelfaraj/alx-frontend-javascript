dce72dd305a9114bbff694fc0d18c57d8aaa6770
abouelfaraj <aef.ayoub@gmail.com>
2024-03-07 13:31:18 +0000

    cmt task 2

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,50 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    if (typeof(name) !== 'string') {
+      throw new typeError("Name must be a string");
+    }
+    if (typeof(length) !== 'number') {
+      throw new typeError("Length must be a number");
+    }
+    if (!Array.isArray(students)) {
+      throw new typeError("Students must be an array of strings");
+    }
+
+    this._name = name;
+    this._length = length;
+    this.students = students;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get length() {
+    return this.length;
+  }
+
+  get students() {
+    return this.students;
+  }
+  
+  set name(name) {
+    if (typeof(name) !== 'string') {
+      throw new TypeError("Name must be a string");
+    }
+    this._name = name;
+  }
+
+  set length(length) {
+    if (typeof(length) !== 'number') {
+      throw new TypeError("Length must be a number");
+    }
+    this._length = length;
+  }
+  
+  set students(students) {
+    if (!Array.isArray(students)) {
+      throw new TypeError("Students must be an array of strings");
+    }
+    this._students = students;
+  }
+}
