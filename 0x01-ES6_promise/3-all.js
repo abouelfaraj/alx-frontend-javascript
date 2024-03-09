d40c061198277726f25a434c4c253e60e4384c4d
abouelfaraj <aef.ayoub@gmail.com>
2024-03-09 12:48:23 +0000

    task 3

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,5 +1,5 @@
 /* eslint-disable */
-import { uploadPhoto, createUser } from './utils.js';
+import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()])
