98d716dae1456fe979f1b47549d02cc8ee76d45d
abouelfaraj <aef.ayoub@gmail.com>
2024-03-12 12:30:40 +0000

    10-update_uniq_items.js

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -2,7 +2,7 @@ export default function updateUniqueItems(map) {
   if (map instanceof Map) {
     for (const [key, value] of map) {
       if (value === 1) {
-	map.set(key, 100);
+        map.set(key, 100);
       }
     }
   } else {
