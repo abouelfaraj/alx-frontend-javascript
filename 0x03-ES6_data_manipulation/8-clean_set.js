fd07abf6ad967f768b954cf2d3d35672195a6ca7
abouelfaraj <aef.ayoub@gmail.com>
2024-03-12 12:05:49 +0000

    cmt task 8

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,8 +1,8 @@
 export default function cleanSet(set, startString) {
   let txt = '';
   const array = [];
-  
-  if (startString && typeof startString ==='string') {
+
+  if (startString && typeof startString === 'string') {
     for (const elem of set) {
       if (elem && elem.startsWith(startString)) {
         array.push(elem.slice(startString.length));
