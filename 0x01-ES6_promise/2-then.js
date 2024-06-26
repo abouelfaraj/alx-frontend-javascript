81222f6c2b43b7ac5e8e8f46a51e4ceace4f82b9
aefayoub <aef.ayoub@gmail.com>
2024-06-26 12:19:54 +0000

    cmt task 2

---
+++ 0x01-ES6_promise/2-then.js
@@ -0,0 +1,8 @@
+/* eslint-disable */
+export default function handelReponseFromAPI(success) {
+  return promise
+    .then(() => ({ status: 200, body: 'success' }))
+    .catch(() => new Error())
+    .finally(() => console.log('Got a response from the API'));
+}
+
