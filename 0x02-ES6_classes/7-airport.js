959a0c2765b923fefeafe1623b8b1920f033a606
aefayoub <aef.ayoub@gmail.com>
2024-06-28 15:47:49 +0000

    cmt task 72 git push

---
+++ 0x02-ES6_classes/7-airport.js
@@ -0,0 +1,15 @@
+class Airport {
+  constructor(name, code) {
+    if (typeof name !== 'string') {
+      throw new TypeError('');
+    } else if (typeof code !== 'string') {
+      throw new TypeError('');
+    }
+    this._name = name;
+    this._code = code;
+  }
+
+  get [Symbol.toStringTag]() {
+    return `${this._code}`;
+  }
+}
